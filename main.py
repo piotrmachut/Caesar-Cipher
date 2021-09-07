7e12f4618315e8e4647dc6f3f3e625b259b8a231
piotrmachut <piotr.machut@gmail.com>
2021-09-07 12:19:03 +0200

    Encryption feature

---
+++ main.py
@@ -0,0 +1,18 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+def encrypt(plain_text, shift_amount):
+  cipher_text = ""
+  for letter in plain_text:
+    position = alphabet.index(letter)
+    if position + shift_amount > 25:
+      shifted_letter = alphabet[(position-26)+shift_amount]
+    else:
+      shifted_letter = alphabet[position+shift_amount]
+    cipher_text += shifted_letter
+  print(f"The encoded text is: {cipher_text}")
+
+encrypt(text, shift)
